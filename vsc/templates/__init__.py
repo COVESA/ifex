3b004dfeb3d3c3a9a2b683974aaa90be2e1be61b
Michael Tsukerman <miketsukerman@gmail.com>
2022-09-01 15:19:38 +0200

    vsc-tools: refactor python module structure
    * Add python setuptools setup.py script.
    * Move python code to the vsc folder.
    * Move templates folder to the vsc folder.
    * Install vscgen tool to simplify codegenerator use via cmd.
    * Update github actions (use python 3.10 and vscgen).
    
    Signed-off-by: Mikhail Tsukerman <mikhail.tcukerman@daimler.com>
    Signed-off-by: Magnus Feuer <magnus.feuer@mercedes-benz.com>

---
+++ vsc/templates/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: Copyright (c) 2022 MBition GmbH.
+# All rights reserved.
+# SPDX-License-Identifier: MPL-2.0
+
+import os
+
+TemplatePath = os.path.dirname(os.path.realpath(__file__))
