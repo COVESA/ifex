508587425c828be6a452df529409446af18e58ec
Michael Tsukerman <miketsukerman@gmail.com>
2022-09-07 11:26:35 +0200

    vscgen: update command line interface
    In vscgen tool template argument becomes optional. When vscgen
    <yaml.file> called it would use default templates from
    vsc.templates.default_templates dictionary.
    
    Signed-off-by: Mikhail Tsukerman <mikhail.tcukerman@daimler.com>
    Signed-off-by: Magnus Feuer <magnus.feuer@mercedes-benz.com>

--- vsc/model/vsc_ast.py
+++ vsc/model/vsc_ast.py
@@ -2,7 +2,7 @@
 # All rights reserved.
 # SPDX-License-Identifier: MPL-2.0
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List, Optional, Dict, Any
 import yaml, dacite
 
@@ -11,7 +11,7 @@ import yaml, dacite
 class Argument:
     name: str
     datatype: str
-    description: Optional[str] = None
+    description: Optional[str] = field(default_factory=lambda: str())
     arraysize: Optional[str] = None
     range: Optional[str] = None
 
@@ -19,7 +19,7 @@ class Argument:
 @dataclass
 class Error:
     datatype: str
-    description: Optional[str] = None
+    description: Optional[str] = field(default_factory=lambda: str())
     arraysize: Optional[str] = None
     range: Optional[str] = None
 
@@ -28,23 +28,23 @@ class Error:
 class Method:
     name: str
     description: Optional[str] = None
-    error: Optional[List[Error]] = None
-    input: Optional[List[Argument]] = None
-    output: Optional[List[Argument]] = None
+    error: Optional[List[Error]] = field(default_factory=lambda: [])
+    input: Optional[List[Argument]] = field(default_factory=lambda: [])
+    output: Optional[List[Argument]] = field(default_factory=lambda: [])
 
 
 @dataclass
 class Event:
     name: str
     description: Optional[str] = None
-    input: Optional[List[Argument]] = None
+    input: Optional[List[Argument]] = field(default_factory=lambda: [])
 
 
 @dataclass
 class Property:
     name: str
     datatype: str
-    description: Optional[str] = None
+    description: Optional[str] = field(default_factory=lambda: str())
     arraysize: Optional[int] = None
 
 
@@ -52,7 +52,7 @@ class Property:
 class Member:
     name: str
     datatype: str
-    description: Optional[str] = None
+    description: Optional[str] = field(default_factory=lambda: str())
     arraysize: Optional[str] = None
 
 
@@ -76,15 +76,15 @@ class Struct:
     name: str
     # TODO: do we need type field in a struct?
     type: Optional[str] = None
-    description: Optional[str] = None
-    members: Optional[List[Member]] = None
+    description: Optional[str] = field(default_factory=lambda: str())
+    members: Optional[List[Member]] = field(default_factory=lambda: [])
 
 
 @dataclass
 class Typedef:
     name: str
     datatype: str
-    description: Optional[str] = None
+    description: Optional[str] = field(default_factory=lambda: str())
     arraysize: Optional[int] = None
     min: Optional[int] = None
     max: Optional[int] = None
@@ -93,25 +93,26 @@ class Typedef:
 @dataclass
 class Include:
     file: str
-    description: Optional[str] = None
+    description: Optional[str] = field(default_factory=lambda: str())
 
 
 @dataclass
 class Namespace:
     name: str
-    description: Optional[str] = None
+    description: Optional[str] = field(default_factory=lambda: str())
 
     major_version: Optional[int] = None
     minor_version: Optional[int] = None
 
-    events: Optional[List[Event]] = None
-    methods: Optional[List[Method]] = None
-    typedefs: Optional[List[Typedef]] = None
-    includes: Optional[List[Include]] = None
-    structs: Optional[List[Struct]] = None
-    enumerations: Optional[List[Enumeration]] = None
-    properties: Optional[List[Property]] = None
-    namespaces: Optional[List['Namespace']] = None
+    events: Optional[List[Event]] = field(default_factory=lambda: [])
+    methods: Optional[List[Method]] = field(default_factory=lambda: [])
+    typedefs: Optional[List[Typedef]] = field(default_factory=lambda: [])
+    includes: Optional[List[Include]] = field(default_factory=lambda: [])
+    structs: Optional[List[Struct]] = field(default_factory=lambda: [])
+    enumerations: Optional[List[Enumeration]] = field(default_factory=lambda: [])
+    properties: Optional[List[Property]] = field(default_factory=lambda: [])
+
+    namespaces: Optional[List['Namespace']] = field(default_factory=lambda: [])
 
 
 def read_yaml_file(filename) -> str:
