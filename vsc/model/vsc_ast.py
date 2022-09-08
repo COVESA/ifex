7e03a24bec0c9fef685dc933416424ca0b841bed
Michael Tsukerman <miketsukerman@gmail.com>
2022-09-08 12:19:49 +0200

    vsc_ast: cleanup module
    Making description field empty by default instead of None.
    Removing Optional[..] from the Lists and using default_factory
    assigning empty lists by default to avoid additional checks in the
    templates code.
    
    Signed-off-by: Mikhail Tsukerman <mikhail.tcukerman@daimler.com>
    Signed-off-by: Magnus Feuer <magnus.feuer@mercedes-benz.com>

--- vsc/model/vsc_ast.py
+++ vsc/model/vsc_ast.py
@@ -5,12 +5,15 @@
 from dataclasses import dataclass, field
 from typing import List, Optional
 
+# shortcut to reduce code duplication for default_factory 
+# parameter in field()
+EmptyList = lambda: []
 
 @dataclass
 class Argument:
     name: str
-    datatype: str
-    description: Optional[str] = field(default_factory=lambda: str())
+    datatype: str 
+    description: str = str()
     arraysize: Optional[str] = None
     range: Optional[str] = None
 
@@ -18,7 +21,7 @@ class Argument:
 @dataclass
 class Error:
     datatype: str
-    description: Optional[str] = field(default_factory=lambda: str())
+    description: Optional[str] = str()
     arraysize: Optional[str] = None
     range: Optional[str] = None
 
@@ -27,23 +30,23 @@ class Error:
 class Method:
     name: str
     description: Optional[str] = None
-    error: Optional[List[Error]] = field(default_factory=lambda: [])
-    input: Optional[List[Argument]] = field(default_factory=lambda: [])
-    output: Optional[List[Argument]] = field(default_factory=lambda: [])
+    error: List[Error] = field(default_factory=EmptyList)
+    input: List[Argument] = field(default_factory=EmptyList)
+    output: List[Argument] = field(default_factory=EmptyList)
 
 
 @dataclass
 class Event:
     name: str
-    description: Optional[str] = None
-    input: Optional[List[Argument]] = field(default_factory=lambda: [])
+    description: str = None
+    input: List[Argument] = field(default_factory=EmptyList)
 
 
 @dataclass
 class Property:
     name: str
     datatype: str
-    description: Optional[str] = field(default_factory=lambda: str())
+    description: str = str()
     arraysize: Optional[int] = None
 
 
@@ -51,7 +54,7 @@ class Property:
 class Member:
     name: str
     datatype: str
-    description: Optional[str] = field(default_factory=lambda: str())
+    description: str = str()
     arraysize: Optional[str] = None
 
 
@@ -59,7 +62,7 @@ class Member:
 class Option:
     name: str
     value: int
-    description: Optional[str] = None
+    description: str = None
 
 
 @dataclass
@@ -67,7 +70,7 @@ class Enumeration:
     name: str
     datatype: str
     options: List[Option]
-    description: Optional[str] = None
+    description: str = None
 
 
 @dataclass
@@ -75,15 +78,15 @@ class Struct:
     name: str
     # TODO: do we need type field in a struct?
     type: Optional[str] = None
-    description: Optional[str] = field(default_factory=lambda: str())
-    members: Optional[List[Member]] = field(default_factory=lambda: [])
+    description: str = str()
+    members: List[Member] = field(default_factory=EmptyList)
 
 
 @dataclass
 class Typedef:
     name: str
     datatype: str
-    description: Optional[str] = field(default_factory=lambda: str())
+    description: str = str()
     arraysize: Optional[int] = None
     min: Optional[int] = None
     max: Optional[int] = None
@@ -92,28 +95,28 @@ class Typedef:
 @dataclass
 class Include:
     file: str
-    description: Optional[str] = field(default_factory=lambda: str())
+    description: str = str()
 
 
 @dataclass
 class Namespace:
     name: str
-    description: Optional[str] = field(default_factory=lambda: str())
+    description: str = str()
 
     major_version: Optional[int] = None
     minor_version: Optional[int] = None
 
-    events: Optional[List[Event]] = field(default_factory=lambda: [])
-    methods: Optional[List[Method]] = field(default_factory=lambda: [])
-    typedefs: Optional[List[Typedef]] = field(default_factory=lambda: [])
-    includes: Optional[List[Include]] = field(default_factory=lambda: [])
-    structs: Optional[List[Struct]] = field(default_factory=lambda: [])
-    enumerations: Optional[List[Enumeration]] = field(default_factory=lambda: [])
-    properties: Optional[List[Property]] = field(default_factory=lambda: [])
+    events: List[Event] = field(default_factory=EmptyList)
+    methods: List[Method] = field(default_factory=EmptyList)
+    typedefs: List[Typedef] = field(default_factory=EmptyList)
+    includes: List[Include] = field(default_factory=EmptyList)
+    structs: List[Struct] = field(default_factory=EmptyList)
+    enumerations: List[Enumeration] = field(default_factory=EmptyList)
+    properties: List[Property] = field(default_factory=EmptyList)
 
-    namespaces: Optional[List['Namespace']] = field(default_factory=lambda: [])
+    namespaces: List['Namespace'] = field(default_factory=EmptyList)
+
 
 @dataclass
 class AST(Namespace):
     pass
-
