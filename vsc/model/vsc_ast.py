936e8097ea9c9f84a87e0e31ac386125140d15e9
Michael Tsukerman <miketsukerman@gmail.com>
2022-09-22 09:02:25 +0200

    copyright: fix license headers
    Remove line "#All right reserved" from the files added by previous MR.
    
    Signed-off-by: Mikhail Tsukerman <mikhail.tcukerman@daimler.com>
    Signed-off-by: Magnus Feuer <magnus.feuer@mercedes-benz.com>

--- vsc/model/vsc_ast.py
+++ vsc/model/vsc_ast.py
@@ -1,5 +1,4 @@
 # SPDX-FileCopyrightText: Copyright (c) 2022 MBition GmbH.
-# All rights reserved.
 # SPDX-License-Identifier: MPL-2.0
 
 from dataclasses import dataclass, field
--- vsc/scripts/generator.py
+++ vsc/scripts/generator.py
@@ -1,5 +1,4 @@
 # SPDX-FileCopyrightText: Copyright (c) 2022 MBition GmbH.
-# All rights reserved.
 # SPDX-License-Identifier: MPL-2.0
 
 from vsc.model.vsc_generator import gen
