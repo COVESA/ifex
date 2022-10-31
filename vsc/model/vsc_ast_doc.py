18f6f7e597911b9d8a386b63ae7581ea489368cd
Gunnar Andersson <gunnar_dev@nospam@novaspring.eu>
2022-10-31 18:29:52 +0100

    doc-generation: Include docstrings for each type
    Signed-off-by: Gunnar Andersson <gunnar@nospam@novaspring.eu>

--- vsc/model/vsc_ast_doc.py
+++ vsc/model/vsc_ast_doc.py
@@ -14,7 +14,7 @@ itself (the content of the defined @dataclasses)
 from dataclasses import fields
 from typing import Union, get_origin, get_args
 from vsc.model.vsc_ast import Namespace
-
+import re, itertools
 
 #
 # These helper functions determine the type of object, or they coerce
@@ -100,8 +100,48 @@ def markdown_table_row(field):
         print(f"A list of **{list_member_type_name(field)}**_s_", end='')
     else:
         print(f"A single **{actual_type_name(field)}**", end='')
+
+    print(docstring(field), end='')
     print(" |")
 
+def determine_indentation(s):
+    count = 0
+    # groupby() will collect up repeating characters (like space) so we can
+    # then count the number of spaces to know the indentation.
+    for group in itertools.groupby(s):
+        char = group[0]
+        count = sum(1 for _ in group[1])
+        # NOTE that the string can start with '\n', for example, so instead
+        # of taking the first line we must iterate UNTIL the first indented
+        # line is found.
+        if char == '\t':
+            raise Exception("""Sorry, TABS are not supported by the the
+                    indentation detection code of the documentation
+                    generator!""")
+        # Keep looping until the first group of spaces
+        if char == ' ':
+            break
+    return count
+
+def docstring(item):
+
+    # We can't remove all whitespace at start of every line because this
+    # ruins the indentation of preformatted code blocks.  Instead, figure
+    # out what the indentation level is on the first indented line of the
+    # string, and remove that from all lines:
+
+    s = item.__doc__
+    if s is not None:
+        n = determine_indentation(s)
+
+        # Make a regexp that matches exactly 'n' number of spaces, at the
+        # start of a line, and make sure it is set for multiple lines when
+        # we then use it to substitute
+        p1 = re.compile(f"^ {{{n}}}", re.MULTILINE)
+        # Substitute with empty string, i.e. delete 'n' number of spaces
+        return re.sub(p1, '', s)
+    else:
+        return ""
 
 def markdown_table(fields):
    print(f"|Field Name|Required contents|")
@@ -111,7 +151,11 @@ def markdown_table(fields):
 
 def document_fields(node):
     name = type_name(node)
+
+    print('----') # Separator
     markdown_heading(2, name)
+
+    print(docstring(node))
 
     markdown_heading(4, f"Mandatory fields for {name}:")
     markdown_table([x for x in fields(node) if not is_optional(x.type)])
