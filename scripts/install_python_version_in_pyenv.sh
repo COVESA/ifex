2ffc0f7ffbf463c719fa19225d72fc15ebf3712f
Gunnar Andersson <gunnar@novaspring.eu>
2023-05-12 10:14:25 +0200

    docker: Proper setup, two versions, separate dir
    Some development happening on both Alpine and Ubuntu
    versions. Some challenges compiling from scratch (using pyenv)
    in Alpine version, so it installs from a binary.  The Ubuntu version
    however tests the pyenv method (that is also listed in the README).
    
    - Either way, both variants appear to work.
    - Moved most things into a subdirectory.
    - A Makefile provided for convenience.
    
    Goal: After `make run` it should be possible to run ifexgen without any
    dependency issues.
    
    Signed-off-by: Gunnar Andersson <gunnar_dev@nospam@novaspring.eu>

--- scripts/install_python_version_in_pyenv.sh
+++ scripts/install_python_version_in_pyenv.sh
@@ -6,12 +6,20 @@
 # This file is part of IFEX project
 # ---------------------------------------------------------------------------
 
+# This is local to this file, and basically the setup is buggy if this needs to be used.
+FALLBACK_VERSION=3.10.10
+
 cd "$(dirname "$0")"
+cd ..
+
+# pyenv writes a local .python-version file to remember which is the currently
+# enabled version in the environment. We store .default-python-version in the
+# repository to define the default version used for testing.
 
 # Get python version from stored .python-version file
 # In case the file does not exist, assign a default version
-REQUESTED=$(cat .python-version)
-VERSION=${REQUESTED:-3.10.10}
+DEFAULT_PYTHON_VERSION=$(cat .default-python-version)
+VERSION=${DEFAULT_PYTHON_VERSION:-$FALLBACK_VERSION}
 
 # Install the required version using pyenv.
 pyenv install --skip-existing $VERSION
