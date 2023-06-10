bbd883be3cf4ac8ff6df720dce130fb2d7919ae5
Gunnar Andersson <gunnar_dev@nospam@novaspring.eu>
2023-06-10 02:43:18 +0200

    scripts/generator (ifexgen) updated to work with new structure
    Signed-off-by: Gunnar Andersson <gunnar_dev@nospam@novaspring.eu>

--- ifex/scripts/generator.py
+++ ifex/scripts/generator.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: Copyright (c) 2022 MBition GmbH.
 # SPDX-License-Identifier: MPL-2.0
 
+from ifex.model.ifex_generator import jinja_env
 from ifex.model.ifex_generator import gen
 from ifex.model.ifex_parser import get_ast_from_yaml_file
 import argparse, dacite
@@ -9,6 +10,8 @@ def ifex_generator_run():
     parser = argparse.ArgumentParser(description='Runs IFEX code generator.')
     parser.add_argument('input', metavar='input', type=str,
                         help='input.yaml-file (path)')
+    parser.add_argument('templatedir', metavar='template-dir', type=str, nargs='?',
+                        help='output type defined by the name of the template (sub)directory')
     parser.add_argument('template', metavar='template', type=str, nargs='?',
                         help='output-template-file (name only, not path)')
     try:
@@ -18,9 +21,9 @@ def ifex_generator_run():
 
     ast = get_ast_from_yaml_file(args.input)
 
-    templatename = args.template
-
-    print(gen(ast, templatename))
+    jinja_env.__init__(args.templatedir)
+    jinja_env.set_template_env( gen=gen )
+    print(gen(ast))
 
 
 if __name__ == "__main__":
