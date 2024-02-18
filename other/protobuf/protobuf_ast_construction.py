20ee781545e28493f8833ad0a8befa2fa829a5b6
Gunnar Andersson <gunnar.andersson@mercedes-benz.com>
2024-02-19 00:03:57 +0100

    Protobuf: Create AST model
    Adds protobuf_ast.py and a corresponding protobuf_ast_construction.py,
    similar to ifex_ast_construction.py which facilitates creation of AST
    objects from within a program.
    
    Signed-off-by: Gunnar Andersson <gunnar.andersson@mercedes-benz.com>

---
+++ other/protobuf/protobuf_ast_construction.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 MBition GmbH.
+# SPDX-License-Identifier: MPL-2.0
+
+# This file is part of the IFEX project
+
+"""
+Light-weight support functions for creating the Protobuf tree from program code,
+likely to be used in Protobuf-to/from-IFEX model conversions.
+"""
+
+# See ifex_ast_construction.py for a more in-depth description of what this
+# is and why.  It follows the exact same pattern.
+
+from dataclasses import is_dataclass
+from ifex.model.type_checking_constructor_mixin import add_constructor
+import protobuf_ast
+
+def add_constructors_to_protobuf_ast_model() -> None:
+    """ Mix-in the type-checking constructor support into each of the protobuf_ast classes: """
+    for c in [cls for cls in protobuf_ast.__dict__.values() if
+              isinstance(cls, type) and
+              is_dataclass(cls)]:
+        add_constructor(c)
+
