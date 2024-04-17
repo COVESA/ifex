46d38768cd39c6a3c9e64706334b535d4bf5a8d6
Gunnar Andersson <gunnar.andersson@mercedes-benz.com>
2024-04-17 15:50:57 +0200

    Update setup.py to setuptools from distutils
    Signed-off-by: Gunnar Andersson <gunnar.andersson@mercedes-benz.com>

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
-from distutils import extension
 import glob
-from distutils.core import setup
+from setuptools import setup
 from setuptools import find_packages
 
 from ifex.templates import TemplateDir
