06739326c5a3fe6ff3591d9b2c5b396c2c1f1288
Gunnar Andersson <gunnar.andersson@mercedes-benz.com>
2025-06-27 12:03:54 +0200

    Schema validator: Make it reusable
    - It now returns a proper result code when used as an executable
    - Quiet operation is possible
    - It is also a reusable module, with an entry-function that can be
      called from other python programs.
    
    Signed-off-by: Gunnar Andersson <gunnar.andersson@mercedes-benz.com>
    Signed-off-by: Marco Benelli <marco.benelli@mercedes-benz.com>

--- output_filters/schema/validate.py
+++ output_filters/schema/validate.py
@@ -9,52 +9,74 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 # =======================================================================
 
-import sys
-import yaml
+"""Validate a JSON or YAML file, against a JSON schema"""
+
+from pathlib import Path
 import json
 import jsonschema
-from pathlib import Path
+import sys
+import yaml
 
-input_file = sys.argv[1]
-schema_file = sys.argv[2]
 
-suffix = Path(input_file).suffix
+def schema_check(input_file, schema_file, quiet = True):
 
-# This is a special case to handle also JSON inputs, even if YAML is preferred in most cases
-if suffix == '.json':
-   print("Loading input as JSON")
-   load_function=json.load
-# .yaml or .ifex is preferred, but we assume YAML also if unknown:
-else:
-   print("Loading input as YAML")
-   load_function=yaml.safe_load
-
-# Load input data from file
-with open(input_file, 'r') as file:
-    input_data = load_function(file)
-
-# Load JSON-schema from file
-with open(schema_file, 'r') as file:
-    schema_data = json.load(file)
-
-# Validate YAML data against JSON-schema
-try:
-    jsonschema.validate(input_data, schema_data)
-    print(f"{input_file} is valid according to the schema.")
-except jsonschema.exceptions.ValidationError as e:
-    print(f"\n{input_file} violates the schema.")
-    if len(e.path) == 0:
-        print("The error appears to be at the top-level of the file")
+    if quiet:
+        _print = lambda *args, **kwargs : None
     else:
-        print("The path leading up to the error is:\n")
-        while len(e.path) > 0:
-            node=e.path.popleft()
-            if type(node) is int:
-                # Change indexing from zero-based to one-based and report the item number
-                print(f'[item #{int(node)+1}]->', end="")
-            else:
-                print(f'{node}->')
-    print(f"ERROR: {e.message}")
-    print("\n(Numbers indicate the item number in a list of items.)")
+        _print = print
 
-sys.exit(1)
+    suffix = Path(input_file).suffix
+
+    # This is a special case to handle also JSON inputs, even if YAML is preferred in most cases
+    if suffix == '.json':
+       _print("Loading input as JSON")
+       load_function=json.load
+    # .yaml or .ifex is preferred, but we assume YAML also if unknown:
+    else:
+       _print("Loading input as YAML")
+       load_function=yaml.safe_load
+
+    # Load input data from file
+    with open(input_file, 'r') as file:
+        input_data = load_function(file)
+
+    # Load JSON-schema from file
+    with open(schema_file, 'r') as file:
+        schema_data = json.load(file)
+
+    # Validate data against JSON-schema
+    try:
+        jsonschema.validate(input_data, schema_data)
+        _print(f"{input_file} is valid according to the schema.")
+        return True
+    except jsonschema.exceptions.ValidationError as e:
+        _print(f"\n{input_file} violates the schema.")
+        if len(e.path) == 0:
+            _print("The error appears to be at the top-level of the file")
+        else:
+            _print("The path leading up to the error is:\n")
+            while len(e.path) > 0:
+                node=e.path.popleft()
+                if type(node) is int:
+                    # Change indexing from zero-based to one-based and report the item number
+                    _print(f'[item #{int(node)+1}]->', end="")
+                else:
+                    _print(f'{node}->')
+        _print(f"ERROR: {e.message}")
+        _print("\n(Numbers indicate the item number in a list of items.)")
+        return False
+
+if __name__ == '__main__':
+
+    input_file = sys.argv[1]
+    schema_file = sys.argv[2]
+
+    if len(sys.argv) > 3 and sys.argv[3] == '--quiet':
+        ok = schema_check(input_file, schema_file, quiet=True)
+    else:
+        ok = schema_check(input_file, schema_file, quiet=False)
+
+    if ok:
+        sys.exit(0)
+    else:
+        sys.exit(1)
